a2017f4631c6abbd736239ee3fa1b45c84f69fe3
Mohamed C. <mohamed@chrayed.de>
2024-07-31 20:26:11 +0200

    separate gaming.nix for jovian modules

---
+++ system/gaming.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, pkgs-stable, jovian ... }:
+{
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+    gamescopeSession.enable = true;
+  };
+}
--- system/systempkgs.nix
+++ system/systempkgs.nix
@@ -4,13 +4,6 @@
     enable = true;
     languagePacks = ["de"];
   };
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
-    gamescopeSession.enable = true;
-  };
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   environment.systemPackages = with pkgs; [
