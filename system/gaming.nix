45daf8be3e88523ab4fef46320d1fd20eb9bc38f
Mohamed C. <mohamed@chrayed.de>
2024-07-31 20:29:24 +0200

    install jovian steam

--- system/gaming.nix
+++ system/gaming.nix
@@ -1,10 +1,11 @@
 { config, pkgs, pkgs-stable, jovian ... }:
 {
+  jovian.steam.enable = true;
+  jovian.steam.autoStart = true;
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
-    gamescopeSession.enable = true;
   };
 }
