84c3e5a8280368b1311cec9c8523c260e5848728
Mohamed C. <mohamed@chrayed.de>
2024-08-01 17:49:44 +0200

    remove decky

--- system/gaming.nix
+++ system/gaming.nix
@@ -7,8 +7,6 @@
     desktopSession = "plasma";
   };
 
-  jovian.decky-loader.enable = true;
-  jovian.decky-loader.user = "mo";
   jovian.hardware.has.amd.gpu = true;
   jovian.hardware.amd.gpu.enableEarlyModesetting = true;
   jovian.steamos.enableBluetoothConfig = true;
