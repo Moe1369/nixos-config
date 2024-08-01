847913c73094227d3f9d0e6dfcaca5d71ab50f3f
Mohamed C. <mohamed@chrayed.de>
2024-08-01 11:05:13 +0200

    enable plasma6

--- system/desktop.nix
+++ system/desktop.nix
@@ -1,6 +1,7 @@
 { config, pkgs, pkgs-stable, ... }:
 {
   services.xserver.enable = false;
+  services.desktopManager.plasma6.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.displayManager.sddm.enable = true;
   services.displayManager.autoLogin.enable = true;
