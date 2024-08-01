5028b3aaf1be10a6fc9f99225a92e91d3b0f5896
Mohamed C. <mohamed@chrayed.de>
2024-08-01 10:50:15 +0200

    don't use steamdeck autostart

--- system/desktop.nix
+++ system/desktop.nix
@@ -1,7 +1,8 @@
 { config, pkgs, pkgs-stable, ... }:
 {
   services.xserver.enable = false;
-  services.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.enable = true;
   services.displayManager.autoLogin.enable = true;
   services.displayManager.autoLogin.user = "mo";
 }
