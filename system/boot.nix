51edce131034d13535d59a7c6b05aa9bd6503c86
Mohamed C. <mohamed@chrayed.de>
2024-08-01 16:51:42 +0200

    extra boot settings

--- system/boot.nix
+++ system/boot.nix
@@ -6,7 +6,7 @@
   boot.plymouth.theme = "bgrt";
   boot.initrd.verbose = false;
   boot.consoleLogLevel = 0;
-  boot.kernelParams = [ "quiet" "udev.log_level=0" ];
+  boot.kernelParams = [ "quiet" "udev.log_level=0" "radeon.cik_support=0" "amdgpu.cik_support=1" ];
   boot.extraModprobeConfig = ''
     options amdgpu ppfeaturemask=0xFFF7FFFF
 '';
--- system/systempkgs.nix
+++ system/systempkgs.nix
@@ -7,6 +7,10 @@
   environment.systemPackages =
    (with pkgs; [
     mangohud
+    unigine-valley
+    unigine-heaven
+    unigine-tropics
+
     ])
 
     ++
