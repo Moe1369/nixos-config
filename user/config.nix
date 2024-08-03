2e475005046305e3fe552b769f180a0628f38cff
Mohamed C. <mohamed@chrayed.de>
2024-08-03 18:43:36 +0200

    add colors

--- user/config.nix
+++ user/config.nix
@@ -1,4 +1,6 @@
 { config, pkgs, pkgs-stable, ... }:
 {
-home.file."~/.config/haruna/haruna.conf".source = ../haruna.conf;
+home.file.".config/haruna/haruna.conf".source = ../configs/haruna.conf;
+home.file.".local/share/color-schemes/BreezeDunkelFarbe.colors".source = ../configs/BreezeDunkelFarbe.colors;
+home.file.".local/share/color-schemes/BreezeHellFarbe.colors".source = ../configs/BreezeHellFarbe.colors;
 }
