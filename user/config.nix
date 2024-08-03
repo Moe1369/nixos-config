d3cb8b700eae062865b87cd24b274c9dcf9578f9
Mohamed C. <mohamed@chrayed.de>
2024-08-03 19:19:19 +0200

    more home manager settings

--- user/config.nix
+++ user/config.nix
@@ -3,4 +3,5 @@
 home.file.".config/haruna/haruna.conf".source = ../configs/haruna.conf;
 home.file.".local/share/color-schemes/BreezeDunkelFarbe.colors".source = ../configs/BreezeDunkelFarbe.colors;
 home.file.".local/share/color-schemes/BreezeHellFarbe.colors".source = ../configs/BreezeHellFarbe.colors;
+home.file.".config/kglobalshortcutsrc".source = ../configs/kglobalshortcutsrc;
 }
