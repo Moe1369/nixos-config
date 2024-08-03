da1ff007a494c6ee696a0c26e1d43fbe8d9f3643
Mohamed C. <mohamed@chrayed.de>
2024-08-03 18:38:15 +0200

    wrong haruna filename

--- user/config.nix
+++ user/config.nix
@@ -1,4 +1,4 @@
 { config, pkgs, pkgs-stable, ... }:
 {
-home.file."~/.config/haruna/haruna.config".source = ../configs/haruna.config;
+home.file."~/.config/haruna/haruna.conf".source = ../haruna.conf;
 }
