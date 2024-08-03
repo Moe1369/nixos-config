a40cce2f8d1f50df80fca1a1e1297f98ed3582af
Mohamed C. <mohamed@chrayed.de>
2024-08-03 13:45:58 +0200

    delete removed imports

--- configuration.nix
+++ configuration.nix
@@ -14,12 +14,10 @@
       ./system/boot.nix
       ./system/controller.nix
       ./system/desktop.nix
-      ./system/gaming.nix
       ./system/hardware-configuration.nix
       ./system/hardware.nix
       ./system/locale.nix
       ./system/networking.nix
-      ./system/systempkgs.nix
       ./system/systemsettings.nix
       ./system/updates.nix
       ./system/users.nix
