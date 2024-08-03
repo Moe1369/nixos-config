e5e3639b0efa5ac62d144697db0c6f35d4d93daa
Mohamed C. <mohamed@chrayed.de>
2024-08-03 23:42:25 +0200

    add assets

--- user/config.nix
+++ user/config.nix
@@ -3,5 +3,6 @@
 home.file.".config/haruna/haruna.conf".source = ../configs/haruna.conf;
 home.file.".local/share/color-schemes/BreezeDunkelFarbe.colors".source = ../configs/BreezeDunkelFarbe.colors;
 home.file.".local/share/color-schemes/BreezeHellFarbe.colors".source = ../configs/BreezeHellFarbe.colors;
-home.file.".config/kglobalshortcutsrc".source = ../configs/kglobalshortcutsrc;
+#home.file.".config/kglobalshortcutsrc".source = ../configs/kglobalshortcutsrc;
+home.file."Bilder/Hintergründe/wallpaper-nix.png".source = ../assets/wallpaper-nix.png;
 }
