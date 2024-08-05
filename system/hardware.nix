18c685c70a57aa478df79135653b4d3097f41b58
Mohamed C. <65728018+Moe1369@users.noreply.github.com>
2024-08-05 07:22:53 +0200

    remove lact from hardware.nix

--- system/hardware.nix
+++ system/hardware.nix
@@ -13,18 +13,6 @@
     pulse.enable = true;
   };
   services.libinput.enable = false;
-   # GPU Overcloking
-    environment.systemPackages = with pkgs; [
-    lact
-  ];
-  systemd.services.lact = {
-    description = "AMDGPU Control Daemon";
-    after = ["multi-user.target"];
-    wantedBy = ["multi-user.target"];
-    serviceConfig = {
-      ExecStart = "${pkgs.lact}/bin/lact daemon";
-    };
-    enable = true;
   };
 
 }
