c33135530499cfcccee6392937e47feaaac3ad2a
Mohamed C. <65728018+Moe1369@users.noreply.github.com>
2024-08-05 07:23:16 +0200

    proper lact systemd service

--- system/systemd.nix
+++ system/systemd.nix
@@ -1,4 +1,11 @@
 { ... }:
 {
-systemd.services.lact.enable = true;
+  systemd.services.lact = {
+    description = "AMDGPU Control Daemon";
+    after = ["multi-user.target"];
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      ExecStart = "${pkgs.lact}/bin/lact daemon";
+    };
+    enable = true;
 }
