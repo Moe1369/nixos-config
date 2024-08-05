9fed4e24fa5e72722b7a65d161bab28f9ef34fd4
Mohamed C. <mohamed@chrayed.de>
2024-08-05 19:11:04 +0200

    revert gtk workaround

--- user/usr-plasma.nix
+++ user/usr-plasma.nix
@@ -3,11 +3,6 @@
   programs.plasma = {
     enable = true;
 
-    configFile = {
-      "gtk-3.0/settings.ini"."Settings"."gtk-theme-name" = "Breeze";
-      "gtk-4.0/settings.ini"."Settings"."gtk-theme-name" = "Breeze";
-    };
-
     workspace.wallpaper = "/home/mo/Bilder/Hintergründe/wallpaper-nix.png";
     kscreenlocker.wallpaper = "/home/mo/Bilder/Hintergründe/wallpaper-nix.png";
 
