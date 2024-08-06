3dec7b3a9951e535ca8cb6f87cc775fc21dbd717
Mohamed C. <mohamed@chrayed.de>
2024-08-06 06:41:10 +0200

    revert plymouth override

--- system/set-systemd.nix
+++ system/set-systemd.nix
@@ -8,15 +8,5 @@
     serviceConfig = {
       ExecStart = "${pkgs.lact}/bin/lact daemon";
     };
-   systemd.services.display-manager = {
-     enable = true;
-     conflicts = [ "plymouth-quit.service"];
-     after = [ "plymouth-quit.service" "rc-local.service" "plymouth-start.service" "systemd-user-sessions.service"];
-     OnFailure= ["plymouth-quit.service"];
-     serviceConfig = {
-       ExecStartPost= "sleep 30";
-       ExecStartPost= "plymouth quit --retain-splash";
-     };
-   };
   };
 }
