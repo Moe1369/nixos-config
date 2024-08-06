f96d725558cee61ce6efa608c5b6fd5cc336bedd
Mohamed C. <mohamed@chrayed.de>
2024-08-06 18:54:32 +0200

    change default shell for user

--- system/set-dev.nix
+++ system/set-dev.nix
@@ -1,6 +1,7 @@
 { pkgs, ... }
 :{
   users.defaultUserShell = pkgs.zsh;
+  users.users.mo.shell = pkgs.zsh;
   programs.zsh.enableCompletion = true;
   programs.zsh.ohMyZsh.enable = true;
   programs.zsh.ohMyZsh.theme = "agnoster";
