ff7a56d06c436b3c2c4062ccac166a99d402e1d9
Mohamed C. <mohamed@chrayed.de>
2024-08-08 05:51:43 +0200

    add pkgs

--- packages/pkg-misc.nix
+++ packages/pkg-misc.nix
@@ -3,11 +3,13 @@
   environment.systemPackages =
    (with pkgs; [
     google-chrome
+    teams-for-linux
+    citrix_workspace
     ])
 
     ++
 
    (with pkgs-stable; [
-    teams-for-linux
+
     ]);
 }
