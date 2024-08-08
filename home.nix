1b6bab157732c5f858c3925f795abef64ac8a1ca
Mohamed C. <mohamed@chrayed.de>
2024-08-08 18:36:54 +0200

    import git settings

--- home.nix
+++ home.nix
@@ -9,12 +9,4 @@
   ];
   home.stateVersion = "24.05";
   programs.home-manager.enable = true;
-  programs.git = {
-   enable = true;
-   userName = "Mohamed C.";
-   userEmail = "mohamed@chrayed.de";
-   extraConfig = {
-     init.defaultBranch = "main";
-   };
-  };
 }
