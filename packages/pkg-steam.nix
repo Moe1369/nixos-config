e1e6e88476c1d93194154705a512e9dc5b9a2ba9
Mohamed C. <mohamed@chrayed.de>
2024-08-10 12:26:04 +0200

    refractor steam

--- packages/pkg-steam.nix
+++ packages/pkg-steam.nix
@@ -4,14 +4,7 @@
     enable = true;
   };
 
-  jovian.hardware.has.amd.gpu = true;
-  jovian.hardware.amd.gpu.enableEarlyModesetting = true;
-  jovian.steamos.enableBluetoothConfig = true;
-
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
   };
 }
