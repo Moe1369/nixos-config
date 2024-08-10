d6e43256484cc5372a7796796e84ebf0c2cdf677
Mohamed C. <mohamed@chrayed.de>
2024-08-10 12:58:08 +0200

    remove old folders

--- packages/pkg-desktop.nix
+++
@@ -1,7 +0,0 @@
-{ ... }:
-{
-  services.xserver.enable = false;
-  services.desktopManager.plasma6.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
-  services.displayManager.sddm.enable = true;
-}
--- system/set-networking.nix
+++
@@ -1,6 +0,0 @@
-{ lib, ... }:
-{
-  networking.useDHCP = lib.mkDefault true;
-  networking.hostName = "computer-mo";
-  networking.networkmanager.enable = true;
-}
