bc06438f3da7b97942e484c276413710657f745f
Mohamed C. <mohamed@chrayed.de>
2024-08-16 15:11:24 +0200

    create Code folder with proper icon

--- modules/user/plasma/default.nix
+++ modules/user/plasma/default.nix
@@ -1,5 +1,6 @@
 { ... }:
 {
+  home.file."Code/.directory".source = ../../../dotfiles/directory;
   home.file.".config/haruna/haruna.conf".source = ../../../dotfiles/haruna.conf;
   home.file.".local/share/color-schemes/BreezeDunkelFarbe.colors".source = ../../../dotfiles/BreezeDunkelFarbe.colors;
   home.file.".local/share/color-schemes/BreezeHellFarbe.colors".source = ../../../dotfiles/BreezeHellFarbe.colors;
