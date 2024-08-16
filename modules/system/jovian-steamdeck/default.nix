80303bebb22f8a57b8be8310daf9def8ce0228d4
Mohamed C. <mohamed@chrayed.de>
2024-08-16 17:45:28 +0200

    add jovian for steamdeck

---
+++ modules/system/jovian-steamdeck/default.nix
@@ -0,0 +1,26 @@
+{ user, ... }:
+{
+  jovian.steam = {
+    enable = true;
+    autoStart = true;
+    user = user;
+    desktopSession = "plasma";
+  };
+  jovian.decky-loader.enable = true;
+  jovian.devices.steamdeck = {
+    enable = true;
+    autoUpdate = true;
+    enableControllerUdevRules = true;
+    enableDefaultCmdlineConfig = true;
+    enableDefaultStage1Modules = true;
+    enableFwupdBiosUpdates = true;
+    enableGyroDsuService = true;
+    enableKernelPatches = true;
+    enableOsFanControl = true;
+    enablePerfControlUdevRules = true;
+    enableSoundSupport = true;
+    enableXorgRotation = true;
+  };
+
+
+}
