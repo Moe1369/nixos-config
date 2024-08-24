40c5ce5dd2410b55c5423b203c946133ed188103
Mohamed C. <mohamed@chrayed.de>
2024-08-24 11:13:25 +0200

    auto upgrade for all hosts

--- modules/system/upgrades/default.nix
+++ modules/system/upgrades/default.nix
@@ -1,9 +1,9 @@
-{ ... }:
+{ hostName, ... }:
 {
 system.autoUpgrade = {
     enable = true;
     dates = "04:00";
-    flake = "github:Moe1369/nixos-config";
+    flake = "github:Moe1369/nixos-config#${hostName}";
     flags = [
         "--update-input" "nixpkgs" "--commit-lock-file"
     ];
