a2f182dda587731ed90ab279bd3436038e5ac2c3
Mohamed C. <mohamed@chrayed.de>
2024-08-25 10:35:50 +0200

    add steam autostart service

--- modules/system/steam/default.nix
+++ modules/system/steam/default.nix
@@ -1,5 +1,17 @@
 { pkgs, ... }:
 {
+
+  systemd.user.services.steam = {
+    enable = true;
+    description = "Open Steam in the background at boot";
+    serviceConfig = {
+      ExecStart = "${pkgs.steam}/bin/steam -nochatui -nofriendsui -silent %U";
+      wantedBy = [ "graphical-session.target" ];
+      Restart = "on-failure";
+      RestartSec = "5s";
+    };
+  };
+
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
