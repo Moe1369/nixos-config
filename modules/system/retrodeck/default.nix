b26ede124eed9e3dcd40b04547b1feeaaef1422b
Mohamed C. <mohamed@chrayed.de>
2024-08-25 19:00:36 +0200

    enable flatpak update

--- modules/system/retrodeck/default.nix
+++ modules/system/retrodeck/default.nix
@@ -1,6 +1,7 @@
 { ... }:
 {
   services.flatpak.enable = true;
+  services.flatpak.update.onActivation = true;
   services.flatpak.packages = [
     { appId = "net.retrodeck.retrodeck"; origin = "flathub";  }
   ];
