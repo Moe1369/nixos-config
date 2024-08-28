87524503d89b34bda8fa8b0c1f9009678fc7e540
Mohamed C. <mohamed@chrayed.de>
2024-08-28 15:57:30 +0200

    add boot settings for lanzaboote

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -7,8 +7,11 @@
   boot.consoleLogLevel = 0;
   boot.kernelParams = [ "quiet" "udev.log_level=0" ];
   boot.initrd.verbose = false;
-  boot.lanzaboote = {
-    enable = true;
-    pkiBundle = "/etc/secureboot";
-  };
+ # boot.lanzaboote = {
+ #   enable = true;
+ #   pkiBundle = "/etc/secureboot";
+ # };
+  environment.systemPackages = [
+    pkgs.sbctl
+  ];
 }
