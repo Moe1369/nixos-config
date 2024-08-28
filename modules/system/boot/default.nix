6faee239c1f68cd1467ea549d568b02d6fc80432
Mohamed C. <mohamed@chrayed.de>
2024-08-28 15:58:06 +0200

    revert lanzaboote

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -6,12 +6,4 @@
   boot.plymouth.theme = "bgrt";
   boot.consoleLogLevel = 0;
   boot.kernelParams = [ "quiet" "udev.log_level=0" ];
-  boot.initrd.verbose = false;
- # boot.lanzaboote = {
- #   enable = true;
- #   pkiBundle = "/etc/secureboot";
- # };
-  environment.systemPackages = [
-    pkgs.sbctl
-  ];
 }
