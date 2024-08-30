f530b8cab509c9cd90f87fe5ed68a9b748475ace
Mohamed C. <mohamed@chrayed.de>
2024-08-30 22:54:25 +0200

    indent

--- modules/system/upgrades/default.nix
+++ modules/system/upgrades/default.nix
@@ -1,21 +1,21 @@
 { hostName, ... }:
 {
-nix.optimise.automatic = true;
-nix.settings.auto-optimise-store = true;
-nix.optimise.dates = [ "03:45" ];
-system.autoUpgrade = {
-    enable = true;
-    dates = "04:00";
-    persistent = true;
-    flake = "github:Moe1369/nixos-config#${hostName}";
-    flags = [
-        "--update-input" "nixpkgs"
-        "--update-input" "home-manager"
-        "--update-input" "jovian"
-        "--update-input" "nix-flatpak"
-        "-L"
-        "--commit-lock-file"
-    ];
-    allowReboot = true;
-};
+    nix.optimise.automatic = true;
+    nix.settings.auto-optimise-store = true;
+    nix.optimise.dates = [ "03:45" ];
+    system.autoUpgrade = {
+        enable = true;
+        dates = "04:00";
+        persistent = true;
+        flake = "github:Moe1369/nixos-config#${hostName}";
+        flags = [
+            "--update-input" "nixpkgs"
+            "--update-input" "home-manager"
+            "--update-input" "jovian"
+            "--update-input" "nix-flatpak"
+            "-L"
+            "--commit-lock-file"
+        ];
+        allowReboot = true;
+    };
 }
