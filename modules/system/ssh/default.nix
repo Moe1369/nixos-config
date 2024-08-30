659cc31bc449ae0cc1f31b8e5ed0d80ee6ab386a
Mohamed C. <mohamed@chrayed.de>
2024-08-30 22:27:45 +0200

    ssh is a system setting not user

--- modules/system/ssh/default.nix
+++ modules/system/ssh/default.nix
@@ -1,4 +1,11 @@
-{ ... }:
+{ user, ... }:
 {
-    services.openssh.enable = true;
+    services.openssh = {
+        enable = true;
+        settings.PasswordAuthentication = false;
+        settings.KbdInteractiveAuthentication = false;
+    };
+    users.users.${user}.openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEun1YkEfxTpcsQk7ZIwuVTcSsVHjwuKA7xOW4TAi3SE mo@workstation"
+    ];
 }
--- modules/user/ssh/default.nix
+++
@@ -1,6 +0,0 @@
-{ user, ... }:
-{
-users.users.${user}.openssh.authorizedKeys.keys = [
-"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEun1YkEfxTpcsQk7ZIwuVTcSsVHjwuKA7xOW4TAi3SE mo@workstation"
-];
-}
