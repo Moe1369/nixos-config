c5e4657e692b383a90b5a77937062fd7d96e5390
Mohamed C. <mohamed@chrayed.de>
2024-08-31 13:03:17 +0200

    decky loader install and sync

--- modules/system/syncthing/default.nix
+++ modules/system/syncthing/default.nix
@@ -1,4 +1,25 @@
-{ ... }:
+{ user, ... }:
 {
-
+services = {
+  syncthing = {
+    enable = true;
+    user = user;
+    dataDir = "/home/${user}/Sync";
+    configDir = "/home/${user}/.config/syncthing";
+    overrideDevices = true;
+    overrideFolders = true;
+    settings = {
+      devices = {
+        "workstation" = { id = "3TLJKSV-QURWERW-PZ7FUI4-LXL22PU-CFXXWRN-UBWTSPK-BJLHVWD-VMHEOQ2"; };
+       # "device2" = { id = "DEVICE-ID-GOES-HERE"; };
+      };
+      folders = {
+        "Decky-Loader" = {
+          path = "/var/lib/decky-loader";
+          devices = [ "workstation" ];
+        };
+      };
+    };
+  };
+};
 }
