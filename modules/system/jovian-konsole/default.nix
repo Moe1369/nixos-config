9b2874f0b8a0a0d5aabbf1f9497b608faa7031b3
Mohamed C. <mohamed@chrayed.de>
2024-09-15 11:54:58 +0200

    revert amd setting

--- modules/system/jovian-konsole/default.nix
+++ modules/system/jovian-konsole/default.nix
@@ -8,5 +8,7 @@
   };
   jovian.decky-loader.enable = true;
   jovian.decky-loader.user = user;
+  jovian.hardware.has.amd.gpu = true;
+  jovian.hardware.amd.gpu.enableEarlyModesetting = true;
   jovian.steamos.enableBluetoothConfig = true;
 }
