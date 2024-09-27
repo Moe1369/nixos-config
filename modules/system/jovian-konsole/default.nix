c8252a5ddb9e2a18e27b2fadca9c4584902fa48f
Mohamed C. <mohamed@chrayed.de>
2024-09-27 16:39:33 +0200

    remove amd stuff for jovian

--- modules/system/jovian-konsole/default.nix
+++ modules/system/jovian-konsole/default.nix
@@ -8,7 +8,5 @@
   };
   jovian.decky-loader.enable = true;
   jovian.decky-loader.user = user;
-  jovian.hardware.has.amd.gpu = true;
-  jovian.hardware.amd.gpu.enableEarlyModesetting = true;
   jovian.steamos.enableBluetoothConfig = true;
 }
