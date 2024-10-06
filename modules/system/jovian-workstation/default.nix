0dbd3e9f4b27e84a7a6587848f3f2bcb3927d852
Mohamed C. <mohamed@chrayed.de>
2024-10-06 15:13:23 +0200

    disable jovian for workstation

--- modules/system/jovian-workstation/default.nix
+++ modules/system/jovian-workstation/default.nix
@@ -1,13 +1,4 @@
-{ user, ... }:
+{ ... }:
 {
-  jovian.steam = {
-    enable = true;
-    autoStart = false;
-    user = user;
-    desktopSession = "plasma";
-  };
-  jovian.decky-loader.enable = true;
-  jovian.decky-loader.user = user;
-  jovian.steamos.enableBluetoothConfig = true;
-  hardware.enableRedistributableFirmware = true;
+
 }
