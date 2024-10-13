132e4af49a46e1ee461310bca62aec8ff8b6abbd
Mohamed C. <mohamed@chrayed.de>
2024-10-13 14:33:41 +0200

    Change ZSH settings

--- modules/system/shell/default.nix
+++ modules/system/shell/default.nix
@@ -2,6 +2,9 @@
 {
   programs.zsh.enable = true;
   programs.zsh.enableCompletion = true;
+  programs.zsh.syntaxHighlighting.enable = true;
+  programs.zsh.autosuggestions.enable = true;
+  programs.zsh.autosuggestions.async = true;
   programs.zsh.ohMyZsh.enable = true;
   programs.zsh.ohMyZsh.theme = "agnoster";
   users.defaultUserShell = pkgs.zsh;
