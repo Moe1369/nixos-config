01e53f812be6955f37d4c4e2b9bd2cc29f36bcfd
Mohamed C. <mohamed@chrayed.de>
2024-12-09 22:15:14 +0100

    bootloader timeout

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -3,6 +3,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.consoleMode = "max";
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.timeout = 0;
   boot.plymouth.enable = true;
   boot.plymouth.theme = "bgrt";
   boot.consoleLogLevel = 0;
