ca0059de436f5a04d1e701e92a93bb15e3abf389
Mohamed C. <mohamed@chrayed.de>
2024-12-10 13:44:54 +0100

    plymouth settings

--- modules/system/jovian-workstation/default.nix
+++ modules/system/jovian-workstation/default.nix
@@ -1,14 +1,11 @@
 { user, ... }:
 {
-  #substituters = https://jovian-nixos.cachix.org;
-  #trusted-public-keys = jovian-nixos.cachix.org-1:mAWLjAxLNlfxAnozUjOqGj4AxQwCl7MXwOfu7msVlAo=;
-
   jovian.steam.enable = true;
   jovian.steam.autoStart = false;
   jovian.steam.user = user;
   jovian.steam.desktopSession = "plasma";
   jovian.decky-loader.enable = false;
-  jovian.steam.updater.splash = "steamos";
+  jovian.steam.updater.splash = "jovian";
   jovian.hardware.amd.gpu.enableEarlyModesetting = true;
   jovian.devices.steamdeck.enable = true;
   jovian.devices.steamdeck.autoUpdate = true;
