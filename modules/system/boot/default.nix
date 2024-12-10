56cfe19db3c7f9ef5cec6ff74e9ab8f114ee157c
Mohamed C. <mohamed@chrayed.de>
2024-12-10 13:39:18 +0100

    disable plymouth

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -4,8 +4,8 @@
   boot.loader.systemd-boot.consoleMode = "max";
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.timeout = 0;
-  boot.plymouth.enable = true;
-  boot.plymouth.theme = "bgrt";
+  #boot.plymouth.enable = true;
+  #boot.plymouth.theme = "bgrt";
   boot.consoleLogLevel = 0;
   boot.kernelParams = [ "quiet" "udev.log_level=0" ];
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
