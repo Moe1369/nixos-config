07ade7637d74813e06fe5e966f80a5141dd0d4a1
Mohamed C. <mohamed@chrayed.de>
2024-12-10 13:41:52 +0100

    plymouth experiments

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -4,7 +4,7 @@
   boot.loader.systemd-boot.consoleMode = "max";
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.timeout = 0;
-  #boot.plymouth.enable = true;
+  boot.plymouth.enable = true;
   #boot.plymouth.theme = "bgrt";
   boot.consoleLogLevel = 0;
   boot.kernelParams = [ "quiet" "udev.log_level=0" ];
