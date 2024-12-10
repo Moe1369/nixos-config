eaba7925e06213dc4a04644c17e2ed50ceb59df1
Mohamed C. <mohamed@chrayed.de>
2024-12-10 14:10:05 +0100

    change boot settings

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -7,8 +7,9 @@
   boot.plymouth.enable = true;
   boot.plymouth.theme = "bgrt";
   boot.consoleLogLevel = 0;
-  boot.kernelParams = [ "quiet" "udev.log_level=0" ];
+  boot.kernelParams = [ "quiet" "splash" "boot.shell_on_fail" "i915.fastboot=1" "loglevel=3" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" ];
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.verbose = false;
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
