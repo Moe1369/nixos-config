7129adda5af38918c40b46f33f9fc8722a30f192
Mohamed C. <mohamed@chrayed.de>
2024-12-10 14:44:46 +0100

    add extest

--- modules/system/controller/default.nix
+++ modules/system/controller/default.nix
@@ -1,5 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  environment.systemPackages =
+   (with pkgs; [
+    extest
+    ]);
+
   # Ignore Dualsense Touchpad in Desktop
   services.udev.extraRules =''
     ACTION=="add|change", KERNEL=="event[0-9]*", ATTRS{name}=="*Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
