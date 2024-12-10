6f6be6530b9d6698b6bb14a1e5ce6b67bcd24e29
Mohamed C. <mohamed@chrayed.de>
2024-12-10 15:37:53 +0100

    add hashed password

--- modules/system/users/default.nix
+++ modules/system/users/default.nix
@@ -1,6 +1,7 @@
 { user, ... }:
 {
   users.users.${user} = {
+    hashedPassword = "$y$j9T$qziHkyBuG215vEKwqmoFl1$Pd1zqAsFlx1.kENKSn7BCWA1vHTLF2wlq7BQjFxgTu8";
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
   };
