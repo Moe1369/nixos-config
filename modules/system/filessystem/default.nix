fe67cdb62a7851b07578063e37c51ae93ae0f686
Mohamed C. <mohamed@chrayed.de>
2024-12-10 13:09:49 +0100

    add filesystem

---
+++ modules/system/filessystem/default.nix
@@ -0,0 +1,18 @@
+{ ... }:
+{
+    fileSystems."/" =
+    { device = "/dev/disk/by-partlabel/root";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-partlabel/EFI";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  swapDevices = [ {
+    device = "/var/lib/swapfile";
+    size = 16*1024;
+  }];
+}
