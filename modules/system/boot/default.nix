2dcac6fa8a0a1abb03e8e1b030e6a9ca07b5152e
Mohamed C. <mohamed@chrayed.de>
2024-12-10 14:21:44 +0100

    add plymouth themes

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -4,8 +4,6 @@
   boot.loader.systemd-boot.consoleMode = "max";
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.timeout = 0;
-  boot.plymouth.enable = true;
-  boot.plymouth.theme = "bgrt";
   boot.consoleLogLevel = 0;
   boot.kernelParams = [ "quiet" "splash" "boot.shell_on_fail" "i915.fastboot=1" "loglevel=3" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" ];
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
@@ -13,8 +11,14 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
-  environment.systemPackages =
-   (with pkgs; [
-      adi1090x-plymouth-themes
-    ]);
+    boot.plymouth = {
+      enable = true;
+      theme = "connect";
+      themePackages = with pkgs; [
+        # By default we would install all themes
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [ "connect" ];
+        })
+      ];
+    };
 }
