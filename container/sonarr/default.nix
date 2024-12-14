33d5000c3d92b7ab68d80d0f0bf8cfebec30db2e
Mohamed C. <mohamed@chrayed.de>
2024-12-14 18:36:49 +0100

    container add

---
+++ container/sonarr/default.nix
@@ -0,0 +1,15 @@
+{ ... }:
+{
+  virtualisation.oci-containers.containers."sonarr-app" = {
+    autoStart = true;
+    image = "lscr.io/linuxserver/sonarr:latest";
+    environment = {
+      "TZ" = "Europe/Berlin";
+    };
+    volumes = [
+      "vl-sonarr-config:/config:rw"
+      #"/daten/arr-suite:/daten/arr-suite:rw"
+    ];
+    ports = ["7878:7878"];
+  };
+}
