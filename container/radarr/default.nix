297fd9fc9d423e0364076d2e12e2928c70587d4c
Mohamed C. <mohamed@chrayed.de>
2024-12-14 17:45:10 +0100

    container changes

--- container/radarr/default.nix
+++ container/radarr/default.nix
@@ -7,7 +7,7 @@
   };
   virtualisation.oci-containers.backend = "docker";
 
-  virtualisation.oci-containers.containers."ct-radarr-app" = {
+  virtualisation.oci-containers.containers."radarr-app" = {
     autoStart = true;
     image = "lscr.io/linuxserver/radarr:latest";
     environment = {
@@ -17,9 +17,7 @@
       #"/daten/arr-suite:/daten/arr-suite:rw"
       "vl-radarr-config:/config:rw"
     ];
-    log-driver = "journald";
     extraOptions = [
-      "--network-alias=ct-radarr-app"
       "--network=nw-external"
       "--network=nw-internal"
     ];
