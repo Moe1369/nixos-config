66cfdbf6204be184084e143226e6d46ed6f1220f
Mohamed C. <mohamed@chrayed.de>
2024-12-14 20:53:50 +0100

    add recyclarr

---
+++ container/recyclarr/default.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+{
+  system.activationScripts.network-recyclarr = ''
+    ${pkgs.docker}/bin/docker network create network-recyclarr
+  '';
+  virtualisation.oci-containers.containers."container-recyclarr-app" = {
+    autoStart = true;
+    image = "ghcr.io/recyclarr/recyclarr:latest";
+    environment = {
+      "TZ" = "Europe/Berlin";
+    };
+    networks = [
+      "network-recyclarr"
+      "network-sonarr"
+      "network-radarr"
+    ];
+    volumes = [
+      "volume-recyclarr-config:/config:rw"
+    ];
+  };
+}
