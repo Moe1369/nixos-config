9c073d04900878785c288a7ac6e81c3498c5dc61
Mohamed C. <mohamed@chrayed.de>
2024-12-15 12:03:10 +0100

    add bazarr

---
+++ container/bazarr/default.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+{
+  system.activationScripts.network-bazarr = ''
+    ${pkgs.docker}/bin/docker network create network-bazarr
+  '';
+  virtualisation.oci-containers.containers."container-bazarr-app" = {
+    autoStart = true;
+    image = "lscr.io/linuxserver/bazarr:latest";
+    environment = {
+      "TZ" = "Europe/Berlin";
+    };
+    networks = [
+      "network-bazarr"
+    ];
+    volumes = [
+      "volume-bazarr-config:/config:rw"
+      #"/daten/arr-suite:/daten/arr-suite:rw"
+    ];
+    ports = ["6767:6767"];
+  };
+}
