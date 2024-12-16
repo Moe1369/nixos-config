2810c6009e518b031f08dbe27c3720887baa8753
Mohamed C. <mohamed@chrayed.de>
2024-12-16 20:29:52 +0100

    update

--- container/sabnzbd/default.nix
+++ container/sabnzbd/default.nix
@@ -5,6 +5,8 @@
     image = "lscr.io/linuxserver/sabnzbd:latest";
     environment = {
       "TZ" = "Europe/Berlin";
+      "PUID" = "1000";
+      "PGID" = "100";
     };
     networks = [
       "network-internal"
