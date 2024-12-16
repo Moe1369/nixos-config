bd90040cd5df5b20a3bfa974e35d6299ec9186c6
Mohamed C. <mohamed@chrayed.de>
2024-12-16 17:52:28 +0100

    add caddy

---
+++ container/caddy/default.nix
@@ -0,0 +1,21 @@
+{ pkgs, ... }:
+{
+  system.activationScripts.network-caddy = ''
+    ${pkgs.docker}/bin/docker network create network-caddy
+  '';
+  virtualisation.oci-containers.containers."container-caddy-app" = {
+    autoStart = true;
+    image = "lucaslorentz/caddy-docker-proxy:ci-alpine";
+    environment = {
+      "CADDY_INGRESS_NETWORKS" = "network-caddy";
+    };
+    networks = [
+      "network-caddy"
+    ];
+    volumes = [
+      "/var/run/docker.sock:/var/run/docker.sock"
+      "volume-caddy-config:/data:rw"
+    ];
+    ports = ["80:80" "443:443"];
+  };
+}
