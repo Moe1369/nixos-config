f5057de1836b48eb4d85278b63718f3197979009
Mohamed C. <mohamed@chrayed.de>
2024-12-17 02:26:36 +0100

    guc experiments

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -5,7 +5,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.timeout = 0;
   boot.consoleLogLevel = 0;
-  boot.kernelParams = [ "quiet" "splash" "boot.shell_on_fail" "i915.fastboot=1" "loglevel=3" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" "i915.enable_guc=2" "i915.guc_firmware_path=i915/ehl_guc_70.1.1.bin" ];
+  boot.kernelParams = [ "quiet" "splash" "boot.shell_on_fail" "i915.fastboot=1" "loglevel=3" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" "i915.enable_guc=2" "i915.guc_firmware_path=/root/ehl_guc_70.1.1.bin" ];
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.verbose = false;
   boot.initrd.kernelModules = [ ];
