8321114d073877a34f38af5f3f38d704e9b33620
Mohamed C. <mohamed@chrayed.de>
2024-12-17 19:00:37 +0100

    add plex

---
+++ container/plex/default.nix
@@ -0,0 +1,20 @@
+{ ... }:
+{
+  virtualisation.oci-containers.containers."container-plex-app" = {
+    autoStart = true;
+    image = "lscr.io/linuxserver/plex:latest";
+    environment = {
+      "TZ" = "Europe/Berlin";
+      "PUID" = "0";
+      "PGID" = "0";
+    };
+    networks = [
+      "network-internal"
+    ];
+    volumes = [
+      "volume-plex-config:/config:rw"
+      "/data:/data:rw"
+    ];
+    ports = ["8081:80"];
+  };
+}
