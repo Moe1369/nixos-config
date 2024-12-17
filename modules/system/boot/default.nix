896d4f1f8ae95fab73598f5161bbf14e660e9bab
Mohamed C. <mohamed@chrayed.de>
2024-12-17 01:43:58 +0100

    change guc

--- modules/system/boot/default.nix
+++ modules/system/boot/default.nix
@@ -5,7 +5,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.timeout = 0;
   boot.consoleLogLevel = 0;
-  boot.kernelParams = [ "quiet" "splash" "boot.shell_on_fail" "i915.fastboot=1" "loglevel=3" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" "i915.enable_guc=3" ];
+  boot.kernelParams = [ "quiet" "splash" "boot.shell_on_fail" "i915.fastboot=1" "loglevel=3" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" "i915.enable_guc=2" ];
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.verbose = false;
   boot.initrd.kernelModules = [ ];
