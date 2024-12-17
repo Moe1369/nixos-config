62df6d71fc704e0eda916a88f49722ac231d31b3
Mohamed C. <mohamed@chrayed.de>
2024-12-17 17:42:27 +0100

    update

--- container/radarr/default.nix
+++ container/radarr/default.nix
@@ -20,7 +20,7 @@
     labels = {
       "caddy" = "radarr.chrayed.de";
       "caddy.reverse_proxy_0" = "/outpost.goauthentik.io/* http://10.0.10.2:9000";
-      "caddy.forward_auth" = "https://radarr.chrayed.de";
+      "caddy.forward_auth" = "http://10.0.10.2:9000";
       "caddy.forward_auth.uri" = "/outpost.goauthentik.io/auth/caddy";
       "caddy.forward_auth.copy_headers" = "X-Authentik-Username X-Authentik-Groups X-Authentik-Email X-Authentik-Name X-Authentik-Uid X-Authentik-Jwt X-Authentik-Meta-Jwks X-Authentik-Meta-Outpost X-Authentik-Meta-Provider X-Authentik-Meta-App X-Authentik-Meta-Version";
       "caddy.forward_auth.trusted_proxies" = "10.0.0.0/8";
