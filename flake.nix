3aee1cf2220b1bbbd94c39810f24fdc392fff199
Mohamed C. <mohamed@chrayed.de>
2024-12-24 11:08:58 +0100

    switch to plasma

--- flake.nix
+++ flake.nix
@@ -54,14 +54,14 @@
             modules.system.base ++
             modules.system.cpu.amd ++
             modules.system.filesystem.client ++
-            modules.system.gnome ++
-            modules.system.gdm ++
+            modules.system.plasma ++
+            modules.system.sddm ++
             modules.system.gaming ++
             modules.system.jovian.workstation ++ [
               {
                 home-manager.users.${user}.imports =
                   modules.user.base ++
-                  modules.user.gnome ++
+                  modules.user.plasma ++
                   modules.user.gaming;
                 home-manager.extraSpecialArgs = { inherit user; inherit hostName; };
                 home-manager.sharedModules = userModules;
@@ -81,13 +81,13 @@
             modules.system.base ++
             modules.system.cpu.amd ++
             modules.system.filesystem.client ++
-            modules.system.gnome ++
+            modules.system.plasma ++
             modules.system.gaming ++
             modules.system.jovian.konsole ++ [
               {
                 home-manager.users.${user}.imports =
                   modules.user.base ++
-                  modules.user.gnome ++
+                  modules.user.plasma ++
                   modules.user.gaming;
                 home-manager.extraSpecialArgs = { inherit user; inherit hostName; };
                 home-manager.sharedModules = userModules;
@@ -107,13 +107,13 @@
             modules.system.base ++
             modules.system.cpu.amd ++
             modules.system.filesystem.client ++
-            modules.system.gnome ++
+            modules.system.plasma ++
             modules.system.gaming ++
             modules.system.jovian.steamdeck ++ [
               {
                 home-manager.users.${user}.imports =
                   modules.user.base ++
-                  modules.user.gnome ++
+                  modules.user.plasma ++
                   modules.user.gaming;
                 home-manager.extraSpecialArgs = { inherit user; inherit hostName; };
                 home-manager.sharedModules = userModules;
--- modules/system/jovian-workstation/default.nix
+++ modules/system/jovian-workstation/default.nix
@@ -3,7 +3,7 @@
   jovian.steam.enable = true;
   jovian.steam.autoStart = false;
   jovian.steam.user = user;
-  jovian.steam.desktopSession = "gnome";
+  jovian.steam.desktopSession = "plasma";
   jovian.decky-loader.enable = false;
   jovian.hardware.amd.gpu.enableEarlyModesetting = true;
   jovian.steamos.enableBluetoothConfig = true;
