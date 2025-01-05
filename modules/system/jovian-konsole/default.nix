e66f7fef24e6943c77724d69f5f06caa0caec841
Mohamed C. <mohamed@chrayed.de>
2025-01-05 11:03:30 +0100

    add decky to steamdeck and konsole

--- modules/system/jovian-konsole/default.nix
+++ modules/system/jovian-konsole/default.nix
@@ -4,7 +4,8 @@
   jovian.steam.autoStart = true;
   jovian.steam.user = user;
   jovian.steam.desktopSession = "plasma";
-  jovian.decky-loader.enable = false;
+  jovian.decky-loader.enable = true;
+  jovian.decky-loader.user = user;
   jovian.hardware.amd.gpu.enableEarlyModesetting = true;
   jovian.steamos.enableBluetoothConfig = true;
 }
--- modules/system/jovian-steamdeck/default.nix
+++ modules/system/jovian-steamdeck/default.nix
@@ -4,7 +4,8 @@
   jovian.steam.autoStart = true;
   jovian.steam.user = user;
   jovian.steam.desktopSession = "plasma";
-  jovian.decky-loader.enable = false;
+  jovian.decky-loader.enable = true;
+  jovian.decky-loader.user = user;
   jovian.steam.updater.splash = "steamos";
   jovian.hardware.amd.gpu.enableEarlyModesetting = true;
   jovian.devices.steamdeck.enable = true;
