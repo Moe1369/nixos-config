904cbf60f12f46a6bbfeccc5ee16d2d696c443b1
Moe1369 <mohamed@chrayed.de>
2025-01-20 10:05:59 +0100

    fix repo

--- root/services/borg/default.nix
+++ root/services/borg/default.nix
@@ -4,7 +4,7 @@
         paths = "/docker";
         encryption.mode = "none";
         environment.BORG_RSH = "ssh -i /home/administrator/.ssh/extern";
-        repo = "ssh://q1wxyk20@q1wxyk20.repo.borgbase.com/./repo";
+        repo = "ssh://q1wxyk20@q1wxyk20.repo.borgbase.com:repo";
         compression = "auto,zstd";
         startAt = "daily";
     };
