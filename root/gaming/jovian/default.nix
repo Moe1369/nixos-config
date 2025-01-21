e3bfb28747bf2dea91ca9a0475ab63379d37ea70
Moe1369 <mohamed@chrayed.de>
2025-01-21 11:14:15 +0100

    comments

--- root/gaming/jovian/default.nix
+++ root/gaming/jovian/default.nix
@@ -4,14 +4,15 @@
   jovian.steam.enable = true;
   jovian.steam.autoStart = true;
   jovian.steam.user = user;
+  # Needs installed desktop session to make "Return to Desktop" work.
   jovian.steam.desktopSession = "plasma";
   jovian.decky-loader.enable = true;
   jovian.decky-loader.user = user;
   jovian.steam.updater.splash = "steamos";
   jovian.hardware.amd.gpu.enableEarlyModesetting = true;
   jovian.steamos.enableBluetoothConfig = true;
+  # Conditional setting. Only install drivers on steamdeck. 
   jovian.devices.steamdeck = lib.mkIf (config.networking.hostName == "steamdeck") {
     enable = true;
   };
-  #jovian.devices.steamdeck.enable = true;
 }
