425d86ec9f450dc913df8537cfd19a8fabe285a3
Mohamed C. <mohamed@chrayed.de>
2025-02-25 21:13:00 +0100

    remove jovian

--- flake.nix
+++ flake.nix
@@ -35,7 +35,6 @@
     root.filesystem.client = filterNixFiles ./root/filesystem/client;
     root.filesystem.server = filterNixFiles ./root/filesystem/server;
     root.gaming = filterNixFiles ./root/gaming;
-    root.jovian = filterNixFiles ./root/jovian;
     root.gnome = filterNixFiles ./root/gnome;
     root.plasma = filterNixFiles ./root/plasma;
     root.services = filterNixFiles ./root/services;
