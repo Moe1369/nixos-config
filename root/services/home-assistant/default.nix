67fdad90a84e0d29998b8a6dcbacbde5d71fa925
Mohamed C. <mohamed@chrayed.de>
2025-02-26 13:09:49 +0100

    add mqtt

--- root/services/home-assistant/default.nix
+++ root/services/home-assistant/default.nix
@@ -25,4 +25,24 @@
    #   "--pull=always"
    # ];
   };
+  virtualisation.oci-containers.containers."container-home-assistant-mqtt" = {
+    autoStart = true;
+    image = "eclipse-mosquitto:latest";
+    environment = {
+      "TZ" = "Europe/Berlin";
+      "PUID" = "0";
+      "PGID" = "0";
+    };
+    networks = [
+      "network-internal"
+    ];
+    ports = [
+      "1883:1883"
+      "9001:9001"
+    ];
+    volumes = [
+      "volume-home-assistant-mqtt:/etc/mosquitto:rw"
+      "volume-home-assistant-mqtt-config:/mosquitto/config:rw"
+    ];
+  };
 }
