f3ce1952e86e75d14e7048da6f3652ef4617c6b8
Mohamed C. <mohamed@chrayed.de>
2025-02-26 23:15:45 +0100

    enable wake on lan for computer-mo

--- root/base/networking/default.nix
+++ root/base/networking/default.nix
@@ -4,4 +4,5 @@
   networking.hostName = hostName;
   networking.useDHCP = lib.mkDefault true;
   networking.networkmanager.enable = true;
+  networking.interfaces.enp11s0.wakeOnLan.enable = true;
 }
