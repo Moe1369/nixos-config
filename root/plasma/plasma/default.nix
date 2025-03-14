a3659876ace24f93484b2abddbf460986c8b1b2a
Mohamed C. <mohamed@chrayed.de>
2025-03-14 11:26:03 +0100

    disable xserver

--- root/plasma/plasma/default.nix
+++ root/plasma/plasma/default.nix
@@ -2,7 +2,7 @@
 {
   # Install plasma without xserver. Use wayland.
   services.desktopManager.plasma6.enable = true;
-  services.xserver.enable = true;
+  services.xserver.enable = false;
   # Install applications.
   environment.systemPackages =
    (with pkgs; [
