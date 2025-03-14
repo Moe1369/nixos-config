69d71f895a25cb1750a50cc9ab3a2d076e807b0c
Mohamed C. <mohamed@chrayed.de>
2025-03-14 11:27:13 +0100

    disable extest

--- root/gaming/steam/default.nix
+++ root/gaming/steam/default.nix
@@ -3,8 +3,6 @@
 {
   programs.steam = {
     enable = true;
-    # Enable extest so controller works in desktop mode.
-    extest.enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
