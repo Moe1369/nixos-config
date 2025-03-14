b2275104203adbf274e1fe26e1303191f7ead1ec
Mohamed C. <mohamed@chrayed.de>
2025-03-14 10:07:49 +0100

    firewall settings

--- root/gaming/wolf/default.nix
+++ root/gaming/wolf/default.nix
@@ -33,12 +33,4 @@
       "--device-cgroup-rule=c 13:* rmw"
     ];
   };
-  networking.firewall = {
-  enable = true;
-  allowedTCPPorts = [ 47984 47989 47990 48010 ];
-  allowedUDPPortRanges = [
-    { from = 47998; to = 48000; }
-    { from = 8000; to = 8010; }
-  ];
-};
 }
