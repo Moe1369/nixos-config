aa7aa35fedbc2847443ee975ff103d4b0d4ac2ff
Mohamed C. <mohamed@chrayed.de>
2025-04-04 23:06:52 +0200

    vulkan layer

--- root/desktop/graphics/default.nix
+++ root/desktop/graphics/default.nix
@@ -1,6 +1,9 @@
-# Install GPU drivers.
+# Install GPU drivers and vulkan hdr layer
 { ... }:
  {
+  environment.systemPackages = [
+    pkgs.vulkan-hdr-layer-kwin6
+  ];
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
