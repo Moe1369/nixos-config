9df6b70271bce18e67f497678795c58b7a374a1d
Mohamed C. <mohamed@chrayed.de>
2025-04-07 19:40:59 +0200

    add extest

--- root/gaming/desktop/steam/default.nix
+++ root/gaming/desktop/steam/default.nix
@@ -3,6 +3,7 @@
 {
   programs.steam = {
     enable = true;
+    extest.enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
