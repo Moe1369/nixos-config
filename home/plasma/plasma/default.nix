99616dbeacd0053b599c542ba45143f28abc3b52
Mohamed C. <mohamed@chrayed.de>
2025-04-13 13:14:38 +0200

    plasma settings

--- home/plasma/plasma/default.nix
+++ home/plasma/plasma/default.nix
@@ -3,7 +3,6 @@
   home.file."Code/.directory".source = ./dotfiles/code-folder;
   home.file.".config/haruna/haruna.conf".source = ./dotfiles/haruna-config;
   home.file.".local/share/user-places.xbel".source = ./dotfiles/userplaces-${hostName};
-  home.file.".config/kglobalshortcutsrc".source = ./dotfiles/kdeshortcuts-config;
   home.file.".config/kde-material-you-colors/config.conf".source = ./dotfiles/kmyc-config;
   home.file.".config/autostart/kde-material-you-colors.desktop".source = ./dotfiles/kmyc-desktop;
   home.file.wallpaper = {
