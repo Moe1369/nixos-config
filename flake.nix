7ea9772011607b1425e9168f52b9c4007dbe58e3
Mohamed C. <mohamed@chrayed.de>
2025-04-14 12:17:19 +0200

    add flatpak

--- flake.nix
+++ flake.nix
@@ -19,9 +19,12 @@
       url = "github:Jovian-Experiments/Jovian-NixOS/development";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-flatpak ={
+      url ="github:gmodena/nix-flatpak/?ref=latest";
+    };
   };
 
-  outputs = { nixpkgs, home-manager, plasma-manager, nur, jovian, ... }:
+  outputs = { nixpkgs, home-manager, plasma-manager, nur, jovian, nix-flatpak ... }:
   let
     
     system = "x86_64-linux";
@@ -48,6 +51,7 @@
     root.external = [
       home-manager.nixosModules.home-manager
       nur.modules.nixos.default
+      nix-flatpak.nixosModules.nix-flatpak
     ];
     root.jovian = [
       jovian.nixosModules.jovian
--- root/plasma/DE/default.nix
+++ root/plasma/DE/default.nix
@@ -3,6 +3,7 @@
   # Install plasma without xserver. Use wayland.
   services.desktopManager.plasma6.enable = true;
   services.xserver.enable = false;
+  services.flatpak.enable = true;
   # Install applications.
   environment.systemPackages =
    (with pkgs; [
