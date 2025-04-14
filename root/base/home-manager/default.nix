7d0cc6130ed5f27b98aa426e81e4aed7b2402e82
Mohamed C. <mohamed@chrayed.de>
2025-04-14 11:03:39 +0200

    experiments

--- root/base/home-manager/default.nix
+++ root/base/home-manager/default.nix
@@ -4,6 +4,5 @@
     home-manager.useGlobalPkgs = true;
     home-manager.useUserPackages = true;
     home-manager.extraSpecialArgs = { inherit pkgs; inherit user; inherit hostName; };
-    # File extension for conflicting files.
     home-manager.backupFileExtension = "backup";
 }
--- root/gaming/desktop/steam/default.nix
+++ root/gaming/desktop/steam/default.nix
@@ -3,6 +3,25 @@
 {
   programs.steam = {
     enable = true;
+    #Gamescope Fix
+      package = pkgs.steam.override {
+        extraLibraries = pkgs: [ pkgs.xorg.libxcb ];
+        extraPkgs =
+          pkgs: with pkgs; [
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXinerama
+            xorg.libXScrnSaver
+            libpng
+            libpulseaudio
+            libvorbis
+            stdenv.cc.cc.lib
+            libkrb5
+            keyutils
+            gamemode
+          ];
+      };
+    gamescopeSession.enable = true;
     extest.enable = true;
     extraCompatPackages = with pkgs; [
       proton-ge-bin
