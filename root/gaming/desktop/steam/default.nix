6f834e0bac8b702df85203a17eed04103ee115ff
Mohamed C. <mohamed@chrayed.de>
2025-04-16 23:08:13 +0200

    remove gamescope session

--- root/gaming/desktop/steam/default.nix
+++ root/gaming/desktop/steam/default.nix
@@ -1,10 +1,6 @@
 # Steam desktop client.
 { pkgs, ... }:
 {
-  programs.gamescope = {
-    enable = true;
-    capSysNice = true;
-  };
   programs.steam = {
     enable = true;
     extraCompatPackages = with pkgs; [
@@ -13,16 +9,6 @@
     extraPackages = with pkgs;[
       gamescope
       mangohud
-    ];
-    gamescopeSession.enable = true;
-    gamescopeSession.args = [
-      "--hdr-enabled"
-      "--hdr-itm-enable"
-      "--adaptive-sync"
-    ];
-    gamescopeSession.steamArgs = [
-      "-tenfoot"
-      "-pipewire-dmabuf"
     ];
   };
 }
