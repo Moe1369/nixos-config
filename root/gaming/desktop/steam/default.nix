9b5a864a8964900b8861857ca9995915441ac7f0
Mohamed C. <mohamed@chrayed.de>
2025-04-16 14:58:18 +0200

    use jovian

--- root/gaming/desktop/steam/default.nix
+++ root/gaming/desktop/steam/default.nix
@@ -1,29 +1,8 @@
 # Steam desktop client.
-{ pkgs, ... }:
+{ user, ... }:
 {
-  programs.gamescope = {
-    enable = true;
-    capSysNice = true;
-  };
-  programs.steam = {
-    enable = true;
-    extraCompatPackages = with pkgs; [
-      proton-ge-bin
-    ];
-    extraPackages = with pkgs;[
-      gamescope
-      mangohud
-    ];
-    gamescopeSession.enable = true;
-    gamescopeSession.args = [
-      "--hdr-enabled"
-      "--hdr-itm-enable"
-      "--adaptive-sync"
-
-    ];
-    gamescopeSession.steamArgs = [
-      "-tenfoot"
-      "-pipewire-dmabuf"
-    ];
-  };
+  jovian.steam.enable = true;
+  jovian.steam.autoStart = false;
+  jovian.steam.user = user;
+  jovian.steam.desktopSession = "plasma";
 }
