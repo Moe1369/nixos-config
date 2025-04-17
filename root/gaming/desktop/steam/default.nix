e161cb2d60d44841f06db520e1ac4f0b4e53317f
Mohamed C. <mohamed@chrayed.de>
2025-04-18 00:03:37 +0200

    add scripts for steamos

--- root/gaming/desktop/steam/default.nix
+++ root/gaming/desktop/steam/default.nix
@@ -1,7 +1,32 @@
-# Steam desktop client.
 { pkgs, ... }:
+# Scripts for SteamOS session
+let
+  jupiter-biosupdate = pkgs.writeShellScriptBin "jupiter-biosupdate" ''
+    exit 0;
+  '';
+
+  steamos-update = pkgs.writeShellScriptBin "steamos-update" ''
+    exit 7;
+  '';
+
+  steamos-select-branch = pkgs.writeShellScriptBin "steamos-select-branch" ''
+    echo "Not applicable for this OS"
+  '';
+
+  steamos-session-select = pkgs.writeShellScriptBin "steamos-session-select" ''
+    steam -shutdown
+  '';
+in
 {
+  environment.systemPackages = [
+    jupiter-biosupdate
+    steamos-update
+    steamos-select-branch
+    steamos-session-select
+  ];
+
   hardware.steam-hardware.enable = true;
+
   programs.steam = {
     enable = true;
     extest.enable = true;
@@ -12,12 +37,11 @@
     gamescopeSession.steamArgs = [
       "-steamdeck"
       "-steamos3"
-      "-pipewire-dmabuf"
     ];
     extraCompatPackages = with pkgs; [
       proton-ge-bin
     ];
-    extraPackages = with pkgs;[
+    extraPackages = with pkgs; [
       gamescope
       mangohud
       vulkan-hdr-layer-kwin6
--- root/gaming/desktop/steam/script.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-
-  pkgs.writeShellScriptBin "steamos-select-branch" ''
-  echo "Not applicable for this OS
-''
-  pkgs.writeShellScriptBin "steamos-select-branch" ''
-    steam -shutdown
-''
