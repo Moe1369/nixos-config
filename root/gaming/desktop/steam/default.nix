fe6f988b0536050465872da78d9829b02a874e94
Mohamed C. <mohamed@chrayed.de>
2025-04-17 22:21:13 +0200

    steam tweaks

--- root/gaming/desktop/steam/default.nix
+++ root/gaming/desktop/steam/default.nix
@@ -1,8 +1,10 @@
 # Steam desktop client.
 { pkgs, ... }:
 {
+  hardware.steam-hardware.enable = true;
   programs.steam = {
     enable = true;
+    extest.enable = true;
     extraCompatPackages = with pkgs; [
       proton-ge-bin
     ];
