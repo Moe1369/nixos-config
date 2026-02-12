db4ade358c4109e5f68aa59451da5f9d47b1032f
Mohamed C. <mohamed@chrayed.de>
2026-02-12 15:48:37 +0100

    remove ssh keys

--- root/base/ssh/default.nix
+++ root/base/ssh/default.nix
@@ -7,6 +7,6 @@
         settings.KbdInteractiveAuthentication = false;
     };
     users.users.${user}.openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAxNhl9lAA7SGpFv0/YhIrL+C1nrODGsvpXlevjpOP9d Interne Infrastruktur"
+        ""
     ];
 }
