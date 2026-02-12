0a715e41a94d6755f41fb4cb17791e09b689dc97
Mohamed C. <mohamed@chrayed.de>
2026-02-12 15:49:09 +0100

    remove hashed password

--- root/base/users/default.nix
+++ root/base/users/default.nix
@@ -2,7 +2,7 @@
 { user, ... }:
 {
   users.users.${user} = {
-    hashedPassword = "$y$j9T$qziHkyBuG215vEKwqmoFl1$Pd1zqAsFlx1.kENKSn7BCWA1vHTLF2wlq7BQjFxgTu8";
+    hashedPassword = "";
     description = "Mohamed Chrayed";
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
